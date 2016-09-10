feddd81bb42f7878ae1cf1b76bf25e11d2e33d30
Michael <wu.michael.m@gmail.com>
2016-09-10 12:43:28 -0400

    Restructure server files

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ app.use(express.static(__dirname+"/"))
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var clients = require('./clients.js');
+var clients = require('./server/clients.js');
 clients.setIO(io);
 
-var gamerooms = require('./gamerooms.js');
+var gamerooms = require('./server/gamerooms.js');
 gamerooms.setIO(io);
 gamerooms.setClients(clients);
 
-app.get('/' function(req, res){
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var ioConnections = require('./ioconnections.js');
+var ioConnections = require('./server/ioconnections.js');
 ioConnections.init(io, clients, gamerooms);
 
 http.listen(8080, function(){
